a99cece229ff029c813c8016a595dcdfc6391390
Guillaume D. Gth <guillaume.zarb@gmail.com>
2022-07-27 16:24:13 +0200

    branches mgmt

--- branch.js
+++ branch.js
@@ -1,36 +1,39 @@
 const apiUrl = 'https://iv-scrum-api.herokuapp.com'
-const changeBranchStatus = (e) => {
+const changeBranchStatus = (id, status) => {
+    const branch = document.getElementById(id);
+    const branchCode = branch.getAttribute('data-branch-code');
+    const action = status === 'locked' ? 'lock=1' : 'unlock=1';    
+    fetch(`https://project.ivalua.com/page/chg/branch_manage?branch_code=${branchCode}&${action}`, {
+        "headers": {              
+          "content-type": "application/x-www-form-urlencoded",              
+        },                        
+        "body": `old_branch_code=${branchCode}&task_id=133&branch_auto_upgrade=N`,
+        "method": "POST",
+        "mode": "no-cors",
+        "credentials": "include"
+      }) 
+      .then(e => {
+        const data = new FormData();
+        data.append('status', status);
+        fetch(`${apiUrl}/Branch/${id}/updateStatus`,
+        {
+            method: 'POST',
+            body: data,
+            headers: {
+                // 'Content-Type':  'multipart/form-data',
+                'Accept': 'application/json'},
+            // mode: 'no-cors'
+        });             
+        branch.setAttribute('data-status', status)
+    });
+}
+const changeBranchStatusClick = (e) => {
     const target = e.target;
     const branch = target.closest('.branch');
-    const status = branch.getAttribute('data-status');
-    const branchCode = branch.getAttribute('data-branch-code');
-    const newStatus = status === 'locked' ? 'opened' : 'locked';
-    const action = newStatus === 'locked' ? 'lock=1' : 'unlock=1';    
+    const status = branch.getAttribute('data-status'); 
+    const newStatus = status === 'locked' ? 'opened' : 'locked';      
     const id = branch.id;
-    fetch(`https://project.ivalua.com/page/chg/branch_manage?branch_code=${branchCode}&${action}`, {
-            "headers": {              
-              "content-type": "application/x-www-form-urlencoded",              
-            },                        
-            "body": `old_branch_code=${branchCode}&task_id=133&branch_auto_upgrade=N`,
-            "method": "POST",
-            "mode": "no-cors",
-            "credentials": "include"
-          }) 
-          .then(e => {
-            const data = new FormData();
-            data.append('status', newStatus);
-            fetch(`${apiUrl}/Branch/${id}/updateStatus`,
-            {
-                method: 'POST',
-                body: data,
-                headers: {
-                    // 'Content-Type':  'multipart/form-data',
-                    'Accept': 'application/json'},
-                // mode: 'no-cors'
-            });             
-            branch.setAttribute('data-status', newStatus)
-        });
-          
+    changeBranchStatus(id, newStatus);
 }
 const getBranches = () => {
     const myHeaders = new Headers();
@@ -58,8 +61,10 @@ const getBranches = () => {
                 branch.setAttribute('data-status', b.status);
                 branch.setAttribute('id', b.id);
                 branch.setAttribute('data-branch-code', `buyer_V8_${b.version}_D351364_branch`);
-                const version = document.createElement('span');
+                const version = document.createElement('a');
+                version.setAttribute('href', `https://project.ivalua.com/page/chg/branch_manage?branch_code=buyer_V8_${b.version}_D351364_branch`)
                 version.classList.add('version')
+                version.setAttribute('target', 'blank')
                 branch.append(version);
                 version.innerText = b.version;
                 branch.classList.add('branch', 'flex', 'row', );  
@@ -74,7 +79,7 @@ const getBranches = () => {
                 else{
                     icon.classList.add('fa-lock-open');
                 }
-                button.onclick = changeBranchStatus;
+                button.onclick = changeBranchStatusClick;
                 button.append(icon);
                 branch.append(button);
                 branchesElement.append(branch)                
